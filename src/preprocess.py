9f262be0bcbafb6483025eba3c6690b4109122f3
KaushalJyanni <jyanni@iitk.ac.in>
2018-04-18 17:24:25 +0530

    changed preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,13 +2,19 @@ import random
 import string
 import sys
 inp_file = sys.argv[1]
-out_file = sys.argv[2]
+out_file1="t_"+str(inp_file.split("/")[-1])
+out_file2="e_"+str(inp_file.split("/")[-1])
+# out_file = sys.argv[2]
 with open(inp_file,'r') as f:
 	data=f.readlines()
-
-data=random.sample(data,500)
+totalsize=500
+t_setsize=int(0.7*totalsize)
+# e_setsize=0.3*totalsize
+data=random.sample(data,totalsize)
+tdata=data[:t_setsize]
+edata=data[t_setsize:]
 fdata=[]
-for d in data:
+for d in tdata:
 	newstring=""
 	for char in d:
 		if char in string.ascii_letters:
@@ -17,6 +23,19 @@ for d in data:
 			newstring+=" "
 	fdata.append("<s>"+newstring+"</s>\n")
 
-with open(out_file,'w') as f:
+with open(out_file1,'w') as f:
+	for s in fdata:
+		f.write(s)
+fdata=[]
+for d in edata:
+	newstring=""
+	for char in d:
+		if char in string.ascii_letters:
+			newstring+=char.lower()
+		else:
+			newstring+=" "
+	fdata.append("<s>"+newstring+"</s>\n")
+
+with open(out_file2,'w') as f:
 	for s in fdata:
 		f.write(s)
